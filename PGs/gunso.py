f41828a91569e29e32b31ef689457fdde720afe6
kylend2 <kylend2@illinois.edu>
2022-12-13 17:58:17 -0600

    vm testing commit6

--- PGs/gunso.py
+++ PGs/gunso.py
@@ -63,7 +63,7 @@ def config_server_gunso():
     # TODO: make port number acessible
     r1 = requests.get(url_pre + port_num+"/")
     #print(r1.content)
-    print(r1.history)
+    print(r1.links)
     print("UIN")
     print(r1.raw)
     r = requests.put(url_pre + port_num+"/register-pg", json= {"name": "gunso", "author": "kylend2", "secret":"NA"})
